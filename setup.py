64fcf540f81cd791f8b276b443e2655808014a16
sslivkoff <stormslivkoff@gmail.com>
2021-02-02 00:04:06 -0800

    update setup.py version and requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 
 setup(
     name='milp',
-    version='0.0.2',
+    version='1.0',
     packages=['milp'],
     package_dir={'milp': 'milp'},
-    install_requires=[],
     install_requires=[
         'matplotlib',
         'numpy',
