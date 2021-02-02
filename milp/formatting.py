7d3b327c0a4a7c6c079d7e9fd373b86804d5500b
sslivkoff <stormslivkoff@gmail.com>
2021-02-02 00:03:46 -0800

    move initial formatting code outside of notebook

---
+++ milp/formatting.py
@@ -0,0 +1,21 @@
+
+def setup_plot_formatting():
+
+    # adapted from https://stackoverflow.com/a/39566040
+
+    import matplotlib.pyplot as plt
+
+    plt.rcParams['figure.dpi'] = 96
+
+    SMALL_SIZE = 12
+    MEDIUM_SIZE = 14
+    BIGGER_SIZE = 16
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
